bb85117315b5123db44a6a991c59b759f2f9567d
wasiu <nexxyb@gmail.com>
2025-03-28 15:07:10 +0100

    commit

--- cocktail_exchange/settings.py
+++ cocktail_exchange/settings.py
@@ -62,9 +62,13 @@ REST_FRAMEWORK = {
     ],
 }
 
-# CORS settings
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React frontend
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://localhost:3001",
 ]
 
 MIDDLEWARE = [
