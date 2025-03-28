d71e5ef6d7dc53cfe8f3360c81bdee523a7a0156
wasiu <nexxyb@gmail.com>
2025-03-28 16:21:18 +0100

    commit

--- cocktail_exchange/settings.py
+++ cocktail_exchange/settings.py
@@ -68,6 +68,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5000",
+    "http://localhost:5173",
     "http://localhost:3001",
 ]
 
@@ -88,15 +89,6 @@ MIDDLEWARE = [
 #     "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
 # }
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
 
 ROOT_URLCONF = "cocktail_exchange.urls"
 
@@ -224,3 +216,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+if os.environ.get("server") != "local":
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_SSL_REDIRECT = True
